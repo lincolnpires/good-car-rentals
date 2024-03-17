b6705e357d7093804c59736bd20bf956e514d00a
Lincoln Pires <me@lincolnpires.com>
2024-03-17 18:39:58 +0100

    Add dbContext and configure startign point

---
+++ src/Data/CarRentalsContext.cs
@@ -0,0 +1,25 @@
+﻿using GoodCarRentals.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GoodCarRentals.Data;
+
+public class CarRentalsContext: DbContext
+{
+    public DbSet<Car> Cars { get; set; } = default!;
+    public DbSet<Customer> Customers { get; set; } = default!;
+    public DbSet<Rental> Rentals { get; set; } = default!;
+
+    private string DbPath { get; }
+
+    public CarRentalsContext(DbContextOptions<CarRentalsContext> options) : base(options)
+    {
+        var appPath = AppContext.BaseDirectory;
+        DbPath = Path.Combine(appPath, "GoodCarRentals.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={DbPath}");
+    }
+
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,23 +1,32 @@
+using GoodCarRentals.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<CarRentalsContext>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+// From here, all is to Configure the HTTP request pipeline.
 
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<CarRentalsContext>();
+    context.Database.EnsureCreated();
+}
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
+// Have error handler in production as well - avoid different behavior between environments as well
+app.UseExceptionHandler("/Home/Error");
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
