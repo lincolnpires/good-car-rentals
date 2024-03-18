9bd5fcd2d2a03a1a424dfa52e7f544f28d1bd823
Lincoln Pires <me@lincolnpires.com>
2024-03-18 23:21:59 +0100

    Scaffold rentals too
    We'll need the basic views code to speed the logic.

---
+++ src/Controllers/RentalsController.cs
@@ -0,0 +1,158 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GoodCarRentals.Data;
+using GoodCarRentals.Data.Models;
+
+namespace GoodCarRentals.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private readonly CarRentalsContext _context;
+
+        public RentalsController(CarRentalsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rentals
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rentals.ToListAsync());
+        }
+
+        // GET: Rentals/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // GET: Rentals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rentals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RentalDate,ReturnDate,TotalCost,IsReturned")] Rental rental)
+        {
+            if (ModelState.IsValid)
+            {
+                rental.Id = Guid.NewGuid();
+                _context.Add(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rental);
+        }
+
+        // GET: Rentals/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals.FindAsync(id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            return View(rental);
+        }
+
+        // POST: Rentals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,RentalDate,ReturnDate,TotalCost,IsReturned")] Rental rental)
+        {
+            if (id != rental.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rental);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RentalExists(rental.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rental);
+        }
+
+        // GET: Rentals/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rentals
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // POST: Rentals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var rental = await _context.Rentals.FindAsync(id);
+            if (rental != null)
+            {
+                _context.Rentals.Remove(rental);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentalExists(Guid id)
+        {
+            return _context.Rentals.Any(e => e.Id == id);
+        }
+    }
+}
